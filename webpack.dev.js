67df4cca27be804080b89fe5b5d30b1553c89f0f
daniel-j-guenther <53337648+daniel-j-guenther@users.noreply.github.com>
2021-02-17 15:15:05 +0100

    amazing-codebase@1.1.6

--- webpack.dev.js
+++ webpack.dev.js
@@ -1,27 +1,17 @@
 const path = require('path')
 const webpack = require('webpack')
 const HtmlWebPackPlugin = require('html-webpack-plugin')
-const MiniCSSExtractPlugin = require('mini-css-extract-plugin')
-const OptimizeCssAssetsPlugin = require('optimize-css-assets-webpack-plugin')
-const TerserPlugin = require("terser-webpack-plugin")
 const { CleanWebpackPlugin } = require('clean-webpack-plugin')
 const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin
 
 module.exports = {
     mode: 'development',
     entry: './src/client/index.js',
+    devtool: 'source-map',
     output: {
         libraryTarget: 'var',
         library: 'Client'
     },
-    optimization: {
-        minimize: true,
-        minimizer: [
-            new TerserPlugin({}),
-            new OptimizeCssAssetsPlugin({})
-        ]
-    },
-    devtool: 'source-map',
     stats: 'verbose',
     module: {
         rules: [
@@ -32,7 +22,7 @@ module.exports = {
             },
             {
                 test: /\.s?css$/,
-                use: [MiniCSSExtractPlugin.loader, 'css-loader', 'sass-loader'],
+                use: ['style-loader', 'css-loader', 'sass-loader'],
             },
             {
                 test: /\.(png|ttf)$/,
@@ -48,9 +38,6 @@ module.exports = {
         new HtmlWebPackPlugin({
             template: "./src/client/views/index.html",
             filename: "./index.html",
-        }),
-        new MiniCSSExtractPlugin ({
-            filename: '[name].css'
         }),
         new CleanWebpackPlugin({
             // Simulate the removal of files
