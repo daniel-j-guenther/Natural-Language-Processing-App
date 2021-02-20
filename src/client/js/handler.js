7ec2043924bce6ecf9f2160916c9ed5bded75c2a
daniel-j-guenther <53337648+daniel-j-guenther@users.noreply.github.com>
2021-02-20 13:48:19 +0100

    amazing-codebase@1.1.8

--- src/client/js/handler.js
+++ src/client/js/handler.js
@@ -3,12 +3,14 @@ function runHandler(event) {
     let userLink = document.getElementById('website-url').value.toString();
     console.log("::: Web Address Submitted :::");
     Client.runValidator(userLink)
-    fetch('/analysis')
-    .then(res => res.json())
-    .then(function(res) {
-        document.getElementById('nlp-overview').innerHTML = "Natural Language Processing is a subset of Artificial Intellence that provides computers the ability to process natural human speech. Machine Learning and Deep Learning techniques are used on massive amounts of data so that computers can understand human speech.";
-        document.getElementById('nlp-feedback').innerHTML = "The website you provided is " + res.subjectivity + " and " + res.confidence + "% confident on the topic.";
-    })
+    setTimeout(()=>{
+        fetch('/analysis')
+        .then(res => res.json())
+        .then(function(res) {
+            document.getElementById('nlp-overview').innerHTML = "Natural Language Processing is a subset of Artificial Intellence that provides computers the ability to process natural human speech. Machine Learning and Deep Learning techniques are used on massive amounts of data so that computers can understand human speech.";
+            document.getElementById('nlp-feedback').innerHTML = "The contents of this page is " + res.subjectivity + " in nature and " + res.confidence + "% confident on the topic.";
+        })
+    }, 12000)
 }
 
 export { runHandler }
